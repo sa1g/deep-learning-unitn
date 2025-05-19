1ca7e628c1e2bcf6aeecd2a0abc4d13c38a26027
ettore <ettoreitaut4.1@gmail.com>
2025-05-19 22:45:14 +0200

    zero shot

--- zero-shot-open-clip.py
+++ zero-shot-open-clip.py
@@ -1,6 +1,3 @@
-# Accuracy: 49.37%
-# Latency: 141.43 ms
-
 import torch
 
 torch.manual_seed(456)
@@ -8,7 +5,7 @@ torch.cuda.manual_seed(456)
 
 import torch.nn as nn
 import torch.nn.functional as F
-import clip
+import open_clip
 
 
 from src.augmix import AugMixKornia, ImageTransform, kornia_preprocess
@@ -16,33 +13,30 @@ from src.data import ResnetA
 from src.utils import bench
 
 
-
-
 class ClipWrapper(nn.Module):
     def __init__(
-        self, model: nn.Module, prompt: str = "a photo of a {}", device: str = None
+        self, model: nn.Module, prompt: str = "a photo of a {}", device: str = "cuda"
     ):
         super().__init__()
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        self.tokenizer = open_clip.get_tokenizer("ViT-B-16")
         self.model = model
         self.logit_scale = model.logit_scale.data
+        # self.logit_scale = model.log
 
         self.prompt = prompt
 
+    # @torch.compile()
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Forward pass through the model.
-
-        Returns the predicted class.
-        """
         prompt = torch.cat(
             [
-                clip.tokenize(self.prompt.format(c))
+                self.tokenizer(self.prompt.format(c))
                 for c in dataset.class_code_to_label.values()
             ]
         ).to(device)
 
-        with torch.no_grad():
+        with torch.no_grad(), torch.autocast("cuda"):
             image_features = self.model.encode_image(x)
             image_features /= image_features.norm(dim=-1, keepdim=True)
 
@@ -72,7 +66,10 @@ if __name__ == "__main__":
     dataloader, dataset = ResnetA(augmenter)
 
     # Load the CLIP model
-    clip_model, _ = clip.load("ViT-B/16", device=device)
+    clip_model, _, preprocess_val = open_clip.create_model_and_transforms(
+        # model_name="ViT-B-16", pretrained="datacomp_xl_s13b_b90k", device=device#, force_quick_gelu=True
+        model_name="ViT-B-16", pretrained="openai", device=device, force_quick_gelu=True
+    )
     clip_model.eval()
 
     # Create a ClipSkeleton instance
